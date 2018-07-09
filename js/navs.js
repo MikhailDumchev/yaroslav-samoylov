6f9f235a17428f33a521375f3358f7a749089cd0
Apindes <apindesign@yahoo.com>
2018-07-09 19:14:12 +0300

    new edits

---
+++ js/navs.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    
+//    $(".carousel-controls").on("click",function(){
+////        var scope = $(".tab-content")[0];
+////        var tabs = $(scope).find(".tab-pane").removeClass("in active");
+//        setTimeout(function(){
+//            var id = $(".mobile-cat-navigation").find(".active").data("id");
+//            console.log(id);
+//        },1500);
+//        
+//        
+//        
+//    })
+    
+    
+})
+
